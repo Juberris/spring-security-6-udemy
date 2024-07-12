bf368faf4af1ff520549158b3dcff1c6c46a7f3f
juan.berrios <juan.berrios@sii.cl>
2024-07-12 11:46:57 -0400

    04 authentication providers

--- src/main/java/com/debuggeandoideas/app_security/security/SecurityConfig.java
+++ src/main/java/com/debuggeandoideas/app_security/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -26,6 +27,9 @@ public class SecurityConfig {
                         .anyRequest().permitAll())
                 .formLogin(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults());
+        //opcional para evitar error por cors
+        http.cors(AbstractHttpConfigurer::disable);
+        http.csrf(AbstractHttpConfigurer::disable);
         return http.build();
     }
 
@@ -54,10 +58,10 @@ public class SecurityConfig {
     }
 */
 
-  /*  @Bean
+   @Bean
     PasswordEncoder passwordEncoder(){
         return NoOpPasswordEncoder.getInstance();
-    }*/
+    }
 
 
 }
